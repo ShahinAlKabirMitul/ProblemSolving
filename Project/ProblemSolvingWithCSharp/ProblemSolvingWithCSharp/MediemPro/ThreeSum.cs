51b1873e448b237c99711296558b76d2605ec309
Shahin Al Mitul <mitul.cse@gmail.com>
2022-06-01 08:19:50 +0600

    three sum

---
+++ Project/ProblemSolvingWithCSharp/ProblemSolvingWithCSharp/MediemPro/ThreeSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingWithCSharp.MediemPro
+{
+   public static class ThreeSum
+    {
+        public static List<int[]> ThreeNumberSum(int[] array, int targetSum)
+        {
+            // Write your code here.
+            Array.Sort(array);
+            List<int[]> result = new List<int[]>();
+            for (int i = 0; i < array.Length-2; i++)
+            {
+                int left = i+1;
+                int right = array.Length - 1;
+                while (left < right)
+                {
+                    int currentSum = array[i] + array[left] + array[right];
+                    if (currentSum == targetSum)
+                    {
+                        int[] myResult = { array[i], array[left], array[right] };
+                        result.Add(myResult);
+                        left++;
+                        right--;
+                    }
+                    else if (currentSum < targetSum)
+                        left++;
+                    else if (currentSum > targetSum)
+                        right--;
+                }
+            }
+            
+            return new List<int[]>();
+        }
+    }
+}
