a5f8dc220ec48f20bb046b98f749a7b90c2dec25
Shahin Al Mitul <mitul.cse@gmail.com>
2022-06-02 08:10:55 +0600

    SortedSquaredArray

---
+++ Project/ProblemSolvingWithCSharp/ProblemSolvingWithCSharp/EasyProlem/SortedSquaredArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingWithCSharp.EasyProlem
+{
+   public class SortedSquaredArray
+    {
+        public int[] SortedSquaredArrayS1(int[] array)
+        {
+            // Write your code here.
+            int[] result = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                result[i] = array[i] * array[i];
+            }
+            Array.Sort(result);
+            return result;
+        }
+    }
+}
