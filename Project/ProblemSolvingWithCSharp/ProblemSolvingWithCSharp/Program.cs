a1fe1a5510e13cd683336e78e9a8a54ef0ce5a2c
Shahin Al Mitul <mitul.cse@gmail.com>
2022-10-14 17:05:30 +0600

    check string Palindrome

--- Project/ProblemSolvingWithCSharp/ProblemSolvingWithCSharp/Program.cs
+++ Project/ProblemSolvingWithCSharp/ProblemSolvingWithCSharp/Program.cs
@@ -10,10 +10,11 @@ namespace ProblemSolvingWithCSharp
             var ints = new int[2, 4];
 
             var input = new int[] { 1, 2, 3, 5, 6, 8, 9 };
-          //  SortedSquaredArray.Sol3(input);
-            Console.WriteLine(ReverseSting.SOL( "Mitul!"));
+            //  SortedSquaredArray.Sol3(input);
+            // Console.WriteLine(ReverseSting.SOL( "Mitul!"));
+           // Console.WriteLine(Palindrome.Ispalindrome("ABBAtrfgh"));
+            Console.WriteLine(Palindrome.Ispalindrome("ABBAaf"));
 
-            
         }
     }
 }
